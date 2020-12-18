b81570861c32444364c4a105895df6ce80b88c75
kevinyu0506 <kevinyu05062006@gmail.com>
2020-12-18 14:20:24 +0800

    Update test script & test file & Add .gitignore

--- script/test.sh
+++ script/test.sh
@@ -1,4 +1,4 @@
 #!/bin/bash
 
-docker run --rm --net=container:nginx-rtmp -v $(pwd)/../mount:/mount -it linuxserver/ffmpeg -re -i /mount/vod/bunny.mp4 -vcodec libx264 -vprofile baseline -g 30 -acodec aac -strict -2 -f flv rtmp://localhost:1935/stream/bunny
+docker run --rm --net=container:nginx-rtmp -v $(pwd)/../mount:/mount -it linuxserver/ffmpeg -re -i /mount/vod/test_stream.mp4 -vcodec libx264 -vprofile baseline -g 30 -acodec aac -strict -2 -f flv rtmp://localhost:1935/stream/test_stream
 
