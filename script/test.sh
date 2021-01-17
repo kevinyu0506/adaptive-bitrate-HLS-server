94e10eb6b283fe798616f9ccd5a7efda6e183889
kevinyu0506 <kevinyu05062006@gmail.com>
2021-01-17 22:04:27 +0800

    Attempt to add adaptive bit rate function

--- script/test.sh
+++ script/test.sh
@@ -1,4 +1,4 @@
 #!/bin/bash
 
-docker run --rm --net=container:nginx-rtmp -v $(pwd)/../mount:/mount -it linuxserver/ffmpeg -re -i /mount/vod/test_stream.mp4 -vcodec libx264 -vprofile baseline -g 30 -acodec aac -strict -2 -f flv rtmp://localhost:1935/stream/test_stream
+docker run --rm --net=container:nginx-rtmp -v $(pwd)/../mount:/mount -it linuxserver/ffmpeg -re -i /mount/vod/test_stream.mp4 -vcodec libx264 -vprofile baseline -g 30 -acodec aac -strict -2 -f flv rtmp://localhost:1935/live/test_stream
 
