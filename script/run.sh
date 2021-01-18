2fe82084c9df691b32ff7ae2fded33ffff3fabd9
kevin-ct-yu <kevin.ct.yu@linecorp.com>
2021-01-18 17:09:32 +0800

    Update dockerfile & Support for adaptive bit rate streaming

--- script/run.sh
+++ script/run.sh
@@ -4,5 +4,5 @@ cd ..
 
 docker build -t nginx-rtmp .
 
-docker run --rm --name nginx-rtmp -v $(pwd)/mount:/mount -p 8080:80 -p 1935:1935 -d nginx-rtmp
+docker run --rm --name nginx-rtmp -v $(pwd)/mount:/mount -p 8080:80 -p 1935:1935 nginx-rtmp
 
